301ac01a9748d0cec453df6efca30d50778941b5
kaanncavdar <kkagancavdar@gmail.com>
2024-08-25 03:30:15 +0300

    Modal

--- components/uiComponent/Modal.tsx
+++ components/uiComponent/Modal.tsx
@@ -1,4 +1,4 @@
-import React, { use, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import {
     Modal,
     ModalContent,
@@ -18,15 +18,36 @@ import { ListingDetails } from "@/types/lendingDetails";
 import { ethers } from "ethers";
 import nftAbi from "@/context/ContractAbi";
 
+interface ModalProps {
+    children: React.ReactNode;
+    backdrop: 'blur';
+    isOpen: boolean;
+    onOpenChange: () => void;
+}
+
 interface Props {
     isOpen: boolean;
     onOpenChange: () => void;
-    picture: string[];
     currentNft: ListingDetails;
+    picture: { [key: string]: string };
 }
 
-const UIModal = ({ isOpen, onOpenChange, currentNft, picture }: Props) => {
+const MyModal: React.FC<ModalProps> = ({ children, backdrop, isOpen, onOpenChange }) => {
+    const modalStyles = {
+        width: '80%', // Change the width here
+        maxWidth: '500px',
+    };
+
+    return (
+        <Modal backdrop={backdrop} isOpen={isOpen} onOpenChange={onOpenChange} style={modalStyles}>
+            {children}
+        </Modal>
+    );
+};
+
+const UIModal: React.FC<Props> = ({ isOpen, onOpenChange, currentNft, picture }) => {
     const [nftName, setNftName] = useState<string>("");
+
     async function fetchNftName(currentNft?: ListingDetails) {
         if (!currentNft) return;
         const ethersProvider = new ethers.JsonRpcProvider(
@@ -41,76 +62,73 @@ const UIModal = ({ isOpen, onOpenChange, currentNft, picture }: Props) => {
         setNftName(nftName);
         console.log("nftName", nftName);
     }
+
     useEffect(() => {
         fetchNftName(currentNft);
     }, [currentNft]);
+
     return (
-        <>
-            <Modal backdrop="blur" isOpen={isOpen} onOpenChange={onOpenChange}>
-                <ModalContent>
-                    {(onClose) => (
-                        <>
-                            <ModalHeader className="flex flex-col gap-1">
-                                {nftName}
-                            </ModalHeader>
-                            <ModalBody>
-                                
-                                <div className="grid grid-cols-2 gap-4">
-                                    <div>
-                                        <Image
-                                            src={picture[currentNft.nftTokenId]}
-                                            alt="NFT"
-                                            style={{ objectFit: "cover" }}
-                                            width={150}
-                                            height={150}
-                                        />
-                                    </div>
-                                    <div>
-                                        <p>Name: {nftName}</p>
-                                        <p>ID: {currentNft.nftTokenId}</p>
-                                        <p>Amount: {currentNft.loanAmount}</p>
-                                        <p>Fee: {currentNft.interestFee}</p>
-                                        <p>
-                                            Duration: {currentNft.loanDuration}
-                                        </p>
-                                    </div>
+        <MyModal backdrop="blur" isOpen={isOpen} onOpenChange={onOpenChange}>
+            <ModalContent>
+                {(onClose) => (
+                    <>
+                        <ModalHeader className="flex flex-col gap-1">
+                            {nftName}
+                        </ModalHeader>
+                        <ModalBody>
+                            <div className="grid grid-cols-2 gap-4">
+                                <div>
+                                    <Image
+                                        src={picture[currentNft.nftTokenId]}
+                                        alt="NFT"
+                                        style={{ objectFit: "cover" }}
+                                        width={150}
+                                        height={150}
+                                    />
                                 </div>
-                                <p className="text-center">Offers</p>
-                                <Table aria-label="Example static collection table">
-                                    <TableHeader>
-                                        <TableColumn>OFFER NAME</TableColumn>
-                                        <TableColumn>OFFER PRICE</TableColumn>
-                                        <TableColumn>STATUS</TableColumn>
-                                    </TableHeader>
-                                    <TableBody>
-                                        <TableRow key="1">
-                                            <TableCell>LendmeFi-1</TableCell>
-                                            <TableCell>1 ETH</TableCell>
-                                            <TableCell>
-                                                <Button color="success" size="sm">
-                                                    ACCEPT IT
-                                                </Button>{" "}
-                                            </TableCell>
-                                        </TableRow>
-                                    </TableBody>
-                                </Table>
-                            </ModalBody>
-                            <ModalFooter>
-                                <Button
-                                    color="danger"
-                                    variant="light"
-                                    onPress={onClose}
-                                >
-                                    Close
-                                </Button>
-                                <Button color="primary">Offer</Button>
-                                <Button color="primary">Lend</Button>
-                            </ModalFooter>
-                        </>
-                    )}
-                </ModalContent>
-            </Modal>
-        </>
+                                <div>
+                                    <p>Name: {nftName}</p>
+                                    <p>ID: {currentNft.nftTokenId}</p>
+                                    <p>Amount: {currentNft.loanAmount}</p>
+                                    <p>Fee: {currentNft.interestFee}</p>
+                                    <p>Duration: {currentNft.loanDuration}</p>
+                                </div>
+                            </div>
+                            <p className="text-center">Offers</p>
+                            <Table aria-label="Example static collection table">
+                                <TableHeader>
+                                    <TableColumn>OFFER NAME</TableColumn>
+                                    <TableColumn>OFFER PRICE</TableColumn>
+                                    <TableColumn>STATUS</TableColumn>
+                                </TableHeader>
+                                <TableBody>
+                                    <TableRow key="1">
+                                        <TableCell>LendmeFi-1</TableCell>
+                                        <TableCell>1 ETH</TableCell>
+                                        <TableCell>
+                                            <Button color="success" size="sm">
+                                                ACCEPT IT
+                                            </Button>
+                                        </TableCell>
+                                    </TableRow>
+                                </TableBody>
+                            </Table>
+                        </ModalBody>
+                        <ModalFooter>
+                            <Button
+                                color="danger"
+                                variant="light"
+                                onPress={onClose}
+                            >
+                                Close
+                            </Button>
+                            <Button color="primary">Offer</Button>
+                            <Button color="primary">Lend</Button>
+                        </ModalFooter>
+                    </>
+                )}
+            </ModalContent>
+        </MyModal>
     );
 };
 
